6aa48803163f0b07d4f8092dc3df2c8952c78879
AnvarovM <anvarovmuxammadxuja2001@gmail.com>
2022-05-22 23:14:54 +0500

    Added title and dynamic breadcrumb to components

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { appWithTranslation } from 'next-i18next';
 import DarkOrLightState from '../context/DarkOrLightContext';
 import { ThemeProvider } from 'next-themes';
-import Layout from '../components/layout/Layout';
-import { Metadata } from '../components/pages/MetaData';
+import Layout from '@/components/layout/Layout';
+import { Metadata } from '@/components/pages/MetaData';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 import '../styles/globals.css';
 
