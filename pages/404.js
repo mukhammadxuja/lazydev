ef8905bfaa240fa78cd957f7ea112fb3f39051fd
AnvarovM <anvarovmuxammadxuja2001@gmail.com>
2022-05-22 19:40:49 +0500

    Added lazy load to image && added Footer

--- pages/404.js
+++ pages/404.js
@@ -1,20 +1,26 @@
 import React from "react";
-import Image from "next/image";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+
+import { Footer } from "../components/";
 
 const notFound = () => {
   return (
+    <div>
     <div className="flex items-center justify-center h-screen">
       <div className="text-center">
         <h1 className="text-xl font-semibold text-primary dark:text-white">404</h1>
         <p className="text-lg font-semibold text-primary dark:text-white">Page not found</p>
-        <Image
-          src="/page-not-found/page-not-found.png"
+        <LazyLoadImage
+          src="/images/page-not-found/page-not-found.png"
+          effect="blur"
           width={50}
           height={30}
           alt="not found image"
         />
       </div>
+    </div>
+    <Footer />
     </div>
   );
 };
