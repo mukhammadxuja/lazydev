06df6cbdc0e1232c29c650b4ceb3c27acf5abcbe
AnvarovM <anvarovmuxammadxuja2001@gmail.com>
2022-05-28 19:41:49 +0500

    Added error bundries

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { appWithTranslation } from 'next-i18next';
 import { ThemeProvider } from 'next-themes';
 import Layout from '@/components/layout/Layout';
 import { Metadata } from '@/components/pages/MetaData';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 import '../styles/globals.css';
+import ErrorBoundary from '../components/ErrorBoundary';
 
 function MyApp({ Component, pageProps }) {
+  const [errorInfo, setErrorInfo] = useState(null);
   return (
     <ThemeProvider enableSystem={true} attribute="class">
-      <Layout>
-        <Metadata />
-        <Component {...pageProps} />
-      </Layout>
+      <ErrorBoundary>
+        <Layout>
+          <Metadata />
+          <Component {...pageProps} />
+        </Layout>
+      </ErrorBoundary>
     </ThemeProvider>
   );
 }
