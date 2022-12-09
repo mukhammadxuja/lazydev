bde54bca946d0f9a8f57c3af1f905096cf34e7aa
Mukhammadxuja Anvarov <87266381+mukhammadxuja@users.noreply.github.com>
2022-12-09 17:58:52 +0500

    change: footer some code

--- components/footer/index.jsx
+++ components/footer/index.jsx
@@ -19,7 +19,7 @@ const Footer = ({ bg }) => {
           p="py-3 px-4"
           focusAnimation="px-6"
           width="w-[15rem] md:w-[17rem]"
-          placeholder="Search"
+          placeholder={t('home:subscribe')}
         />
       </div>
       <div className="max-w-4xl py-6 md:py-16 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
@@ -66,16 +66,20 @@ const Footer = ({ bg }) => {
           <h3 className="text-xl font-medium text-primary dark:text-white py-4">
             {t('home:footer_team')}
           </h3>
-          <Link className="list-none" href="/docs/components/avatar">
-            <p className="link text-sm">Website</p>
-          </Link>
-          <Link target="_blank" href="">
+          <a
+            className="list-none"
+            href="https://dotsoft.uz"
+            target="_blank"
+            rel="noopener noreferrer">
+            <p className="link text-sm">.Soft Group</p>
+          </a>
+          <Link target="_blank" href="http://t.me/akzmsh">
             <p className="link text-sm">Telegram</p>
           </Link>
         </div>
       </div>
       <div className="max-w-5xl mx-auto flex flex-col md:flex-row space-y-1 md:space-y-0 items-start md:items-center md:justify-between pt-3 pb-5 md:py-10">
-        <p className="text-secondary text-sm">Copyright © 2022 Lazydev</p>
+        <p className="text-secondary text-sm">Copyright ©2022 Lazydev</p>
         <p className="text-secondary text-sm">
           Created By{' '}
           <a
