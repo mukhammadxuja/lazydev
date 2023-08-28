a2b21fb611e9b7b0d6e71fff78f7fb87dee36986
Mukhammadxuja Anvarov <87266381+mukhammadxuja@users.noreply.github.com>
2023-08-28 18:33:42 +0500

    bugfix: navbar padding-x && disabled button select

--- components/docs/components/button/Disabled.jsx
+++ components/docs/components/button/Disabled.jsx
@@ -4,13 +4,13 @@ export const Disabled = () => {
       <button
         type="button"
         disabled
-        className="rounded-lg md:rounded-xl py-2 px-4 md:py-3 md:px-6 text-xs md:text-sm font-medium text-white bg-blue-600 enabled:hover:bg-blue-500 disabled:opacity-75 disabled:cursor-not-allowed enabled:cursor-pointer"
+        className="rounded-lg md:rounded-xl py-2 px-4 md:py-3 md:px-6 text-xs md:text-sm font-medium text-white bg-blue-600 enabled:hover:bg-blue-500 disabled:opacity-75 disabled:cursor-not-allowed enabled:cursor-pointer disabled:select-none"
         >
         Disabled
       </button>
       <button
         type="button"
-        className="rounded-lg md:rounded-xl py-2 px-4 md:py-3 md:px-6 text-xs md:text-sm border font-medium bg-white text-gray-500 shadow-sm cursor-not-allowed dark:bg-gray-800 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400"
+        className="rounded-lg md:rounded-xl py-2 px-4 md:py-3 md:px-6 text-xs md:text-sm border font-medium bg-white text-gray-500 shadow-sm cursor-not-allowed dark:bg-gray-800 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400 disabled:select-none"
         disabled>
         Disabled
       </button>
